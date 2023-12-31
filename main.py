44365b516596fef5a53ee5eb06689178bf79a41d
tokyo <tokyolabs@protonmail.com>
2023-12-31 16:53:58 -0500

    wip

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 class Person:
     def __init__(self, firstname, lastname):
-        self.firtname = firstname
+        self.firstname = firstname
         self.lastname = lastname
 
 
 class Customer(Person):
-    def __init__(self, account_number, balance):
+    def __init__(self, firstname, lastname, account_number, balance):
+        super().__init__(firstname, lastname)
         self.account_number = account_number
         self.balance = balance
 
     def print_info(self):
-        print(f"Cutomer Name: {self.firtname} {self.lastname} \n")
+        print(f"Cutomer Name: {self.firstname} {self.lastname} \n")
         print(f"Account: {self.account_number} \n")
         print(f"Balance: {self.balance} \n")
+
+
+customer_1 = Customer("mike", "cooper", "54324324", 800.80)
+
+customer_1.print_info()
