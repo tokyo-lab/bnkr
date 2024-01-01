4bbbf909376d54a12a57477428d250072b78c34c
tokyo <tokyolabs@protonmail.com>
2023-12-31 20:55:34 -0500

    wip

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import os
+
+
+def clear_screen():
+    # Clear the screen, platform independent
+    os.system("cls" if os.name == "nt" else "clear")
+
+
 class Person:
     def __init__(self, firstname, lastname):
         self.firstname = firstname
@@ -35,10 +43,45 @@ class Customer(Person):
             print("Invalid input. Please enter a numeric value")
 
 
+def start():
+    clear_screen()
+    print("*" * 50)
+    print("*" * 5 + " Welcome to bnkr " + "*" * 5)
+    print("*" * 50)
+    print("\n")
+
+    menu_choice = "x"
+    while not menu_choice.isnumeric() or int(menu_choice) not in range(1, 7):
+        print("Choose an option: ")
+        print(
+            """
+[1] - View My Info
+[2] - Deposit Money
+[3] - Withdraw Money
+[4] - End Program
+              """
+        )
+        menu_choice = input()
+
+    return int(menu_choice)
+
+
+def return_begining():
+    return_choice = "x"
+
+    while return_choice.lower() != "b":
+        return_choice = input("\nPress 'b' to return home")
+
+
+finish_program = False
 customer_1 = Customer("mike", "cooper", "54324324", 800)
-
-customer_1.print_info()
-
-customer_1.deposit()
-
-customer_1.withdraw()
+while not finish_program:
+    menu = start()
+    if menu == 1:
+        customer_1.print_info()
+    elif menu == 2:
+        customer_1.deposit()
+    elif menu == 3:
+        customer_1.withdraw()
+    elif menu == 4:
+        finish_program = True
